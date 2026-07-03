9c287f26e62b03bd58f26095923f39b6ed6c1525
web-crea <liuyan1625@gmail.com>
2026-07-03 11:24:49 -0700

    Update

--- IntellijSource.py
+++
@@ -1,8 +0,0 @@
-uin=input(">>")
-if uin==False:
-    print("Please input REAL inputs!")
-else:
-    if uin=='help':
-        print('See "INTELLIJSOUCE.HSON for help!"')
-    else:
-        print('Contact WEB-CREA for more.')
