74164b679b047662af401f67e0226b6cc8e8d21e
web-crea <liuyan1625@gmail.com>
2026-07-02 18:58:27 -0700

    Create IntellijSource.py

---
+++ IntellijSource.py
@@ -0,0 +1,8 @@
+uin=input(">>")
+if uin==False:
+    print("Please input REAL inputs!")
+else:
+    if uin=='help':
+        print('See "INTELLIJSOUCE.HSON for help!"')
+    else:
+        print('Contact WEB-CREA for more.')
